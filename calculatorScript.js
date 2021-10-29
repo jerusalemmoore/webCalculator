f6e2b1191bf0d3bd45ca7230ced25ff92222fd88
jerusalemmoore <43590688+jerusalemmoore@users.noreply.github.com>
2021-10-29 01:20:49 -0700

    Add files via upload
    Working on js to be able to build numbers on calculator screen.

---
+++ calculatorScript.js
@@ -0,0 +1,12 @@
+let windowVal =document.getElementById('windowVal').innerHTML;
+window.console.log(windowVal);
+function addToEntry(digit){
+  let digitToAdd = parseInt(digit);
+  window.console.log(digitToAdd);
+  if(digitToAdd == 0){
+    windowVal = digitToAdd;
+    window.console.log(windowVal);}
+  else{
+
+  }
+}
