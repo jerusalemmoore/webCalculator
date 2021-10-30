45387b30e7c96d5ac1064e38e59ce8e32d169b2f
jerusalemmoore <43590688+jerusalemmoore@users.noreply.github.com>
2021-10-29 19:33:20 -0700

    Add files via upload
    Added some button press interactivity for the calculator buttons. Simple operations with 2 variables work but still needs work.

--- calculatorScript.js
+++ calculatorScript.js
@@ -1,12 +1,64 @@
-let windowVal =document.getElementById('windowVal').innerHTML;
+let windowVal =document.getElementById('windowVal');
+var var1, var2, op;
 window.console.log(windowVal);
 function addToEntry(digit){
-  let digitToAdd = parseInt(digit);
-  window.console.log(digitToAdd);
-  if(digitToAdd == 0){
-    windowVal = digitToAdd;
-    window.console.log(windowVal);}
+  window.console.log(digit);
+  if(parseInt(windowVal.innerHTML) == 0){
+      windowVal.innerHTML = digit;
+  }
   else{
+    windowVal.innerHTML += digit;
+
+  }
+
+  // windowVal
+}
+function negate(){
+  if(parseInt(windowVal.innerHTML) < 0){
+    windowVal.innerHTML = String(windowVal.innerHTML).replace("-", "");
+  }
+  else if(parseInt(windowVal.innerHTML) == 0){
+    return;
+  }
+  else{
+    windowVal.innerHTML = "-" + windowVal.innerHTML
+  }
+}
+function operation(operation){
+  if(typeof var1 == 'undefined'){
+    var1 = windowVal.innerHTML;
+  }
+  windowVal.innerHTML = 0;
+  console.log(windowVal);
+  op = operation;
+}
+function eval(){
+  if(op == 'undefined'){
+    window.console.log('op not entered')
+    return;
+  }
+  else{
+    var2 = windowVal.innerHTML;
+    var finalAnswer;
+    switch(String(op)){
+      case '+':
+        finalAnswer = var1 + var2;
+        break;
+      case '-':
+        finalAnswer = var1 - var2;
+        break;
+      case '*':
+        finalAnswer = var1 * var2;
+        break;
+      case '/':
+        finalAnswer = var1 / var2;
+        break;
+      default:
+        console.log('error, bad operator');
+    }
+    console.log(var1);
+    console.log(var2);
+    console.log(finalAnswer)
 
   }
 }
